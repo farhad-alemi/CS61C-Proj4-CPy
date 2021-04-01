90bdad88b3dc3328603fbdab7061d8b897f3b337
Farhad Alemi <farhad.alemi@berkeley.edu>
2021-04-01 09:18:42 -0700

    task2_deb_2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def main():
     # Use the setup function we imported and set up the modules.
     # You may find this reference helpful: https://docs.python.org/3.6/extending/building.html
     # YOUR CODE HERE
-    modules = Extension('numc', sources = ['numc.c', 'matrix.c'], extra_compile_args = CFLAGS, extra_link_args = LDFLAGS)
+    modules = Extension('numc', sources = ['src/numc.c', 'src/matrix.c'], extra_compile_args = CFLAGS, extra_link_args = LDFLAGS)
     setup(name = 'numc', ext_modules = [modules])
 
 if __name__ == "__main__":
