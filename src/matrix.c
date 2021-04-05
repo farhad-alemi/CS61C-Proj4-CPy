a04508bc4e5304516c5ef78fceacc06177d1ec38
Farhad Alemi <farhad.alemi@berkeley.edu>
2021-04-05 02:46:00 -0700

    o 128

--- src/matrix.c
+++ src/matrix.c
@@ -307,7 +307,7 @@ int mat_operator(matrix *result, matrix *mat1, matrix *mat2, char operation) {
             }
         }
     } else {
-        omp_set_num_threads(64);
+        omp_set_num_threads(128);
 #pragma omp parallel
         {
             __m256d arr[4];
