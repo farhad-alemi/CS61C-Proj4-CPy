d95824546327b41a1c70e388b3eb6d95b82c3f9a
Farhad Alemi <farhad.alemi@berkeley.edu>
2021-04-09 19:13:55 -0700

    add

--- src/matrix.c
+++ src/matrix.c
@@ -446,12 +446,11 @@ int add_matrix(matrix *result, matrix *mat1, matrix *mat2) {
     dim = result->rows * result->cols;
     threshold = dim / STRIDE * STRIDE;
 
+    omp_set_num_threads(128);
 #pragma omp parallel
     {
         __m256d arr[4];
         double *mat1_data_index, *mat2_data_index, *result_data_index;
-        omp_set_num_threads(64);
-
 #pragma omp for
         for (int index = 0; index < threshold; index += STRIDE) {
             mat1_data_index = mat1_data + index;
