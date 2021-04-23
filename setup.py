987ea87ed9a1f1fabc18f24c455a56dbf6751cef
Farhad Alemi <farhad.alemi@berkeley.edu>
2021-04-22 17:20:53 -0700

    ex matlul

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from distutils.core import setup, Extension
 import sysconfig
 
+
 def main():
-    CFLAGS = ['-g', '-Wall', '-std=c99', '-fopenmp', '-mavx', '-mfma', '-pthread', '-O3']
+    CFLAGS = ['-g', '-Wall', '-std=c99', '-fopenmp',
+              '-mavx', '-mfma', '-pthread', '-O3']
     LDFLAGS = ['-fopenmp']
     # Use the setup function we imported and set up the modules.
     # You may find this reference helpful: https://docs.python.org/3.6/extending/building.html
     # YOUR CODE HERE
-    modules = Extension('numc', sources = ['src/numc.c', 'src/matrix.c'], extra_compile_args = CFLAGS, extra_link_args = LDFLAGS)
-    setup(name = 'numc', ext_modules = [modules])
+    modules = Extension('numc', sources=[
+                        'src/numc.c', 'src/matrix.c'], extra_compile_args=CFLAGS, extra_link_args=LDFLAGS)
+    setup(name='numc', ext_modules=[modules])
+
 
 if __name__ == "__main__":
     main()
