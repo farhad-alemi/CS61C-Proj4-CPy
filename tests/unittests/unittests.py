fb423d64eb1133a1f1a820ff2fa7c685f0be5c6c
Farhad Alemi <farhad.alemi@berkeley.edu>
2021-04-24 10:36:22 -0700

    pow ex

--- tests/unittests/unittests.py
+++ tests/unittests/unittests.py
@@ -235,17 +235,17 @@ class TestPow(TestCase):
         self.assertTrue(is_correct)
         print_speedup(speed_up)
 
-    def test_small_pow11odd(self):
+    def test_small_pow7odd(self):
         # YOUR CODE HERE
         dp_mat, nc_mat = rand_dp_nc_matrix(3, 3, seed=0)
-        is_correct, speed_up = compute([dp_mat, 11], [nc_mat, 11], "pow")
+        is_correct, speed_up = compute([dp_mat, 7], [nc_mat, 7], "pow")
         self.assertTrue(is_correct)
         print_speedup(speed_up)
 
-    def test_small_pow11even(self):
+    def test_small_pow7even(self):
         # YOUR CODE HERE
         dp_mat, nc_mat = rand_dp_nc_matrix(4, 4, seed=0)
-        is_correct, speed_up = compute([dp_mat, 11], [nc_mat, 11], "pow")
+        is_correct, speed_up = compute([dp_mat, 7], [nc_mat, 7], "pow")
         self.assertTrue(is_correct)
         print_speedup(speed_up)
 
